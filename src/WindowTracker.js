da3e968e5f35e327ed7209e5682b0cb5d5fbdb45
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-09 17:00:58 +1100

    Update WindowTracker.js
    Add state to tracker
    add use effect to tracker to watch the state
    If there is an event at the window, then we should watch the width
    And then we should return a function to stop the use effect to re-render the component by telling react to remove the event listener

--- src/WindowTracker.js
+++ src/WindowTracker.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function WindowTracker() {
-  return <h1>Window width: {window.innerWidth}</h1>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function watchWidth() {
+      console.log("Setting up");
+      setWindowWidth(windowWidth);
+    }
+    window.addEventListener("resize", watchWidth);
+
+    return function () {
+      console.log("Cleaning up");
+      window.removeEventListener("resize, watchWidth");
+    };
+  }, []);
+  return <h1>Window width: {windowWidth}</h1>;
 }
