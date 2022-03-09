e018d82192d4008ecaf02e5005b1305bae6a4388
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-09 16:59:23 +1100

    Update App.js
    Create function to toggle and show tracker or not show
    Add button to toggle tracker
    Add component windowtracker if show is true

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
+import WindowTracker from "./WindowTracker";
+import "./App.css";
 
 export default function App() {
   const [show, setShow] = useState(true);
 
+  function toggle() {
+    setShow((prevShow) => !prevShow);
+  }
+
   return (
     <div className="container">
-      <button>Toggle WindowTracker</button>
-      <WindowTracker />
+      <button onClick={toggle}>Toggle WindowTracker</button>
+      {show && <WindowTracker />}
     </div>
   );
 }
